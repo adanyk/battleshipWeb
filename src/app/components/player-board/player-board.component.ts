4ffc8ca6661da6757c56110c53e9c2f7321db8a9
Adam Nykiel <adamigo10000@wp.pl>
2024-02-28 14:59:16 +0100

    added row and column markings

--- src/app/components/player-board/player-board.component.ts
+++ src/app/components/player-board/player-board.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NotebookComponent } from '../notebook/notebook.component';
 
 @Component({
   selector: 'app-player-board',
   standalone: true,
-  imports: [CommonModule, NotebookComponent],
+  imports: [NotebookComponent],
   templateUrl: './player-board.component.html',
   styleUrl: './player-board.component.scss'
 })
