18bc92510a43a99603013e837bd3e65c20baf478
Adam Nykiel <adamigo10000@wp.pl>
2024-03-07 16:55:58 +0100

    gets ships positions from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,40 +3,34 @@ import { RouterOutlet } from '@angular/router';
 import { GameAreaComponent } from './components/game-area/game-area.component';
 import { ShipPosition } from './models/ship-position';
 import { GameService } from './services/game.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameAreaComponent],
+  imports: [RouterOutlet, GameAreaComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title: string = 'battleship';
   gameStarted: boolean = false;
+  APIUrl: string = "https://localhost:7120/Game/start/";
   
-  constructor(private gameService: GameService) {}
+  constructor(private gameService: GameService,
+    private http: HttpClient) {}
 
   startGame() {
     this.gameStarted = true;
-    const [shipsPositionsPlayer1, shipsPositionsPlayer2] = this.getShipsPositions();
-
-    this.gameService.placeShips('Player 1:my-notebook', shipsPositionsPlayer1);
-    this.gameService.placeShips('Player 2:my-notebook', shipsPositionsPlayer2);
+    this.getShipsPositions().subscribe(shipsPositions => {
+      const [shipsPositionsPlayer1, shipsPositionsPlayer2] = shipsPositions;
+      this.gameService.placeShips('Player 1:my-notebook', shipsPositionsPlayer1);
+      this.gameService.placeShips('Player 2:my-notebook', shipsPositionsPlayer2);
+    });
   }
 
-  getShipsPositions(): ShipPosition[][] {
-    return this.mockGetShipsPositions();
-  }
-
-  mockGetShipsPositions(): ShipPosition[][] {
-    return [[
-        { width: '40%', height: '10%', top: '0%', left: '10%'},
-        { width: '10%', height: '30%', top: '0%', left: '0%'}
-      ],[
-        { width: '30%', height: '10%', top: '90%', left: '0%'},
-        { width: '30%', height: '10%', top: '90%', left: '70%'},
-        { width: '30%', height: '10%', top: '80%', left: '50%'}
-    ]];
+  getShipsPositions(): Observable<ShipPosition[][]> {
+    return this.http.get<ShipPosition[][]>(this.APIUrl);
   }
 }
