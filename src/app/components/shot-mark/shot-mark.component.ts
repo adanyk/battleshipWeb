400560d1c044f7d8c51b661f450961db7787fd82
Adam Nykiel <adamigo10000@wp.pl>
2024-03-11 09:48:05 +0100

    enabled shooting indicated map

--- src/app/components/shot-mark/shot-mark.component.ts
+++ src/app/components/shot-mark/shot-mark.component.ts
@@ -9,7 +9,6 @@ import { ShotResult } from '../../models/shot-result';
   styleUrl: './shot-mark.component.scss'
 })
 export class ShotMarkComponent {
-  //@Input() shipId: number = -1; - to represent the ship it belongs to?
   @Input() top: string = "";
   @Input() left: string = "";
   @Input() result: ShotResult = ShotResult.Unfired;
